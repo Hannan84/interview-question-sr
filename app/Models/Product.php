6982623191d955499320ed551f07d2be4057ddfb
Hannan Sarkar <hannansarkar84@gmail.com>
2022-12-06 02:03:53 +0600

    project setup is done

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,8 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = [
-        'title', 'sku', 'description'
-    ];
+//    protected $fillable = [
+//        'title', 'sku', 'description'
+//    ];
+
+    protected $guarded = [];
+
+    public function product_images() {
+        return $this->hasMany('App\Models\ProductImage');
+    }
+    public function product_variants() {
+        return $this->hasMany('App\Models\ProductVariant');
+    }
+    public function product_variant_prices() {
+        return $this->hasMany('App\Models\ProductVariantPrice');
+    }
 
 }
--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class ProductVariant extends Model
 {
 
+    public function product() {
+        return $this->belongsTo('App\Models\Product');
+    }
+    public function variant() {
+        return $this->belongsTo('App\Models\Variant');
+    }
 }
